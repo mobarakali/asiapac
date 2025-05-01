c430e6c3eff84e2853893456abac0b1ff7db794a
Mobarak Ali <mobarak.ali@gmail.com>
2025-05-01 20:19:25 +0600

    Update asiapac.php to improve functionality

--- asiapac.php
+++ asiapac.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Plugin Name: AsiaPac Custom Side Button
- * Plugin URI: https://yourwebsite.com
- * Description: Adds a rotating button on the left side with a custom URL.
+ * Plugin Name: AsiaPac Buttons
+ * Plugin URI: https://github.com/mobarakali/asiapac
+ * Description: Adds a 'Enquire Now' rotating button on the Right side with a custom URL.
  * Version: 1.0
  * Author: Mobarak Ali
  * Author URI: https://github.com/mobarakali
  * License: GPL2
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: asiapac
- * Domain Path: /languages
  *  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -18,25 +17,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 add_action( 'wp_footer', 'asiapac_add_side_button' );
 function asiapac_add_side_button() {
-    $button_url = 'https://yourcustomurl.com'; // <-- REPLACE with your URL
+    $button_url = '/contact/'; // <-- REPLACE with your URL
     ?>
-    <a href="<?php echo esc_url( $button_url ); ?>" target="_blank" class="asiapac-side-button">Click Me</a>
+    <a href="<?php echo esc_url( $button_url ); ?>" target="_blank" class="asiapac-side-button">Enquire Now</a>
     <style>
         .asiapac-side-button {
-            border-radius: 0 0 5px 5px;
+            border-radius: 3px 3px 0 0;
             background:rgb(103, 194, 124);
             color: #fff;
+            cursor: pointer;
             padding: 15px 30px;
-            font-family: sans-serif;
+            font-family: inherit;
+            font-weight: 500;
             font-size: 16px;
+            letter-spacing: 1px;
+            outline: none;
             position: fixed;
-            top: 50%;
-            left: 0;
+            bottom: 50%;
+            right: 0;
             text-decoration: none;
             text-transform: uppercase;
             transform: translateY(-50%) rotate(-90deg);
-            transform-origin: left top;
-            transition: background 0.3s;
+            transform-origin: right bottom;
+            transition: background  .5s ease;
             z-index: 9999;
         }
 
